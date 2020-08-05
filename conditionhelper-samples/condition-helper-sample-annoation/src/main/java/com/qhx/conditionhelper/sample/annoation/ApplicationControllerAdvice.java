dd3c5e5692cfa826c1840da7f91a6e5973af13c7
32415 <hongxinqian@ehai.cn>
2020-08-05 17:11:52 +0800

    fix:解决threadlocal未remove的bug

--- conditionhelper-samples/condition-helper-sample-annoation/src/main/java/com/qhx/conditionhelper/sample/annoation/ApplicationControllerAdvice.java
+++ conditionhelper-samples/condition-helper-sample-annoation/src/main/java/com/qhx/conditionhelper/sample/annoation/ApplicationControllerAdvice.java
@@ -15,6 +15,8 @@ public class ApplicationControllerAdvice {
 
     @ModelAttribute
     public void addAttributes(@RequestParam(required = false) String userId) {
+        // 先清理下threadlocal数据，防止上次因为controller级别就报异常了导致userId没清理完毕导致的脏数据。因为tomcat线程池是循环处理请求的，线程是共享的
+        ConditionContextHolder.conditionContextThreadLocal.remove();
         if(userId != null) {
             UserContext userContext = new UserContext();
             userContext.setUserId(userId);
